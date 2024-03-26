7457a3182d763afe07d52931719ac56650377eda
victor.silva <victorleosilva18@gmail.com>
2024-03-26 16:59:06 -0300

    alteracao do projeto

--- CadastroProdutos/src/app/produtos/produtos.component.ts
+++ CadastroProdutos/src/app/produtos/produtos.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+interface Produto {
+  id: string;
+  nome: string;
+  descricao: string;
+  preco: number;
+  quantidade: number;
+}
+
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.component.html',
-  styleUrl: './produtos.component.css'
+  styleUrls: ['./produtos.component.css']
 })
 export class ProdutosComponent {
-  
+  produtos: Produto[] = [];
+  produto: Produto = {
+    id: '',
+    nome: '',
+    descricao: '',
+    preco: 0,
+    quantidade: 0
+  };
+
+  constructor() { }
+
+  salvarProduto() {
+    this.produtos.push(this.produto);
+    this.produto = {
+      id: '',
+      nome: '',
+      descricao: '',
+      preco: 0,
+      quantidade: 0
+    };
+  }
 }
